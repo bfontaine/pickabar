fc8efd0814ba9197751479e76e4fee864d83fc9d
Baptiste Fontaine <batifon@yahoo.fr>
2016-03-02 18:32:52 +0100

    api: expose a YelpClient class

--- pickabar/api.py
+++ pickabar/api.py
@@ -8,14 +8,14 @@ from netrc import netrc
 from yelp.client import Client
 from yelp.oauth1_authenticator import Oauth1Authenticator
 
-__all__ = ["get_client"]
+__all__ = ["YelpClient"]
 
-def get_client(**kwargs):
+class YelpClient(object):
     """
-    Return a Yelp API client. Keywords arguments should be ``consumer_key``,
-    ``consumer_secret``, ``token``, ``token_secret``. There are passed as-is.
-    If a truthy ``netrc`` is given it'll get the credentials from the
-    ``~/.netrc`` file. They should be in the following format: ::
+    Yelp API client. Keywords arguments for the constructor should be
+    ``consumer_key``, ``consumer_secret``, ``token``, ``token_secret``. There
+    are passed as-is. If a truthy ``netrc`` is given it'll get the credentials
+    from the ``~/.netrc`` file. They should be in the following format: ::
 
         machine api.yelp.com
             login your_consumer_key:your_consumer_secret
@@ -28,26 +28,28 @@ def get_client(**kwargs):
 
     With the example ``~/.netrc`` above one can call this method like so: ::
 
-        >>> client = get_client(netrc=True)
+        >>> client = YelpClient(netrc=True)
 
     Otherwise it'll look like this: ::
 
-        >>> client = get_client(consumer_key="your_consumer_key",
+        >>> client = YelpClient(consumer_key="your_consumer_key",
                 consumer_secret="your_consumer_secret", token="your_token",
                 token_secret="your_token_secret")
 
     If ``netrc`` is passed it takes precedence over the other arguments.
     """
-    if "netrc" in kwargs and kwargs.pop("netrc"):
-        auths = netrc().authenticators("api.yelp.com")
-        if auths:
-            consumer, token, secret = auths
-            key, consumer_secret = consumer.split(":", 1)
-            kwargs.update(
-                consumer_key=key,
-                consumer_secret=consumer_secret,
-                token=token,
-                token_secret=secret,
-            )
 
-    return Client(Oauth1Authenticator(**kwargs))
+    def __init__(self, **kwargs):
+        if "netrc" in kwargs and kwargs.pop("netrc"):
+            auths = netrc().authenticators("api.yelp.com")
+            if auths:
+                consumer, token, secret = auths
+                key, consumer_secret = consumer.split(":", 1)
+                kwargs.update(
+                    consumer_key=key,
+                    consumer_secret=consumer_secret,
+                    token=token,
+                    token_secret=secret,
+                )
+
+        self._client = Client(Oauth1Authenticator(**kwargs))
